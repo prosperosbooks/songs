706fef9d59d7ea9c0e6bd578eafaf7e2b003f1d1
Emin <allen.emin@gmail.com>
2019-01-25 02:03:14 -0500

    second commit wired up react-redux components

---
+++ src/components/SongList.js
@@ -0,0 +1,16 @@
+import React, { Component } from "react";
+import { connect } from "react-redux";
+
+class SongList extends Component {
+  render() {
+    return <div>SongList</div>;
+  }
+}
+
+const mapStateToProps = state => {
+  console.log(state);
+
+  return state;
+};
+
+export default connect(mapStateToProps)(SongList);
